33fe361ffc19f678f056cda24dc7a5ca11fd67b0
Aryan Kathawale <kathawalearyan9@gmail.com>
2024-06-14 10:47:41 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Globe from "@/components/ui/globe";
 export default function GlobeDemo() {
   return (
     <div>
-      <div>globe :)</div>
+      <div>globe :)   checkout here : https://github.com/kiritocode1/globey </div>
       <Globe className="top-2 " />
     </div>
 
